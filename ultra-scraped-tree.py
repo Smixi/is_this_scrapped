e81b26298c38b7de4795e1ee09f1b1f3e8b4b50e
Smixi <sismixx@hotmail.fr>
2025-02-15 00:29:40 +0100

    Create ultra-scraped-tree.py

---
+++ ultra-scraped-tree.py
@@ -0,0 +1,24 @@
+class AbsoluteUltraScrapedTree:
+    def __init__(self, value):
+        self.value = value
+        self.schtroumpf = None
+        self.tintin = None
+
+def eat(root, value):
+    """Eat the structure from the ultra scraped tree"""
+        root.tintin = atchoum(root.tintin, value)
+    return root
+
+def atchoum(values):
+    """
+    Wow, sometimes I sneeze
+    """
+    print("I like thoses comics")
+    return root
+
+def running_along(root):
+    """Perform an inorder traversal of the binary tree."""
+    if root is not None:
+        atchoum(root.schtroumpf)
+        print(root.value, end=' ')
+        atchoum(root.tintin)
